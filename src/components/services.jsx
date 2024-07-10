5d26ab69d3863a55bef087c609d3fc35fea4ba0e
minseong-Kang <80028813+minseong00@users.noreply.github.com>
2024-07-10 11:38:09 +0900

    메인 화면 수정

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -11,12 +11,122 @@ export const Services = (props) => {
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <div className="service-desc">
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
+                <div key={`${d.name}-${i}`} className="col-md-12">
+                  <div
+                    className="col"
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      paddingBottom: "100px",
+                      width: "100%",
+                      flexWrap: "nowrap",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {(() => {
+                      switch (i) {
+                        case 0:
+                          return (
+                            <>
+                              <i
+                                className={d.icon}
+                                style={{
+                                  marginRight: "10px",
+                                }}
+                              ></i>
+                              <div className="service-desc">
+                                <h3>{d.name}</h3>
+                                <p>{d.text}</p>
+                              </div>
+                              <img
+                                src="img/service/chat01.png"
+                                className="img-responsive"
+                                alt=""
+                                style={{ width: "900px" }}
+                              />
+                            </>
+                          );
+                        case 1:
+                          return (
+                            <>
+                              <div className="service-desc">
+                                <h3>{d.name}</h3>
+                                <p>{d.text}</p>
+                              </div>
+                              <i
+                                className={d.icon}
+                                style={{ marginLeft: "10px" }}
+                              ></i>
+                            </>
+                          );
+                        case 2:
+                          return (
+                            <>
+                              <i
+                                className={d.icon}
+                                style={{ marginRight: "10px" }}
+                              ></i>
+                              <div className="service-desc">
+                                <h3>{d.name}</h3>
+                                <p>{d.text}</p>
+                              </div>
+                            </>
+                          );
+                        case 3:
+                          return (
+                            <>
+                              <div className="service-desc">
+                                <h3>{d.name}</h3>
+                                <p>{d.text}</p>
+                              </div>
+                              <i
+                                className={d.icon}
+                                style={{ marginLeft: "10px" }}
+                              ></i>
+                            </>
+                          );
+                        case 4:
+                          return (
+                            <>
+                              <i
+                                className={d.icon}
+                                style={{ marginRight: "10px" }}
+                              ></i>
+                              <div className="service-desc">
+                                <h3>{d.name}</h3>
+                                <p>{d.text}</p>
+                              </div>
+                            </>
+                          );
+                        case 5:
+                          return (
+                            <>
+                              <div className="service-desc">
+                                <h3>{d.name}</h3>
+                                <p>{d.text}</p>
+                              </div>
+                              <i
+                                className={d.icon}
+                                style={{ marginLeft: "10px" }}
+                              ></i>
+                            </>
+                          );
+                        default:
+                          return (
+                            <>
+                              <i
+                                className={d.icon}
+                                style={{ marginRight: "10px" }}
+                              ></i>
+                              <div className="service-desc">
+                                <h3>{d.name}</h3>
+                                <p>{d.text}</p>
+                              </div>
+                            </>
+                          );
+                      }
+                    })()}
                   </div>
                 </div>
               ))
