aa907e53eda0f560b0bb105ca42c26227a2b1d16
MEONJIYEON <108587227+MEONJIYEON@users.noreply.github.com>
2024-07-11 14:28:33 +0900

    Admin Add
    Admin Add

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
 import { About } from "./components/about";
 import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
 import { Info } from "./components/Info";
 import Request from "./components/Request";
-
+import { Admin } from "./components/Admin";
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
@@ -18,25 +17,43 @@ export const scroll = new SmoothScroll('a[href*="#"]', {
 
 const App = () => {
   const [landingPageData, setLandingPageData] = useState({});
+  const [status, setStatus] = useState("main");
   useEffect(() => {
     setLandingPageData(JsonData);
   }, []);
 
+  const renderContent = () => {
+    switch (status) {
+      case "main":
+        return (
+          <>
+            {/* 메인 화면 */}
+            <Header data={landingPageData.Header} />
+            {/* 회사 소개 */}
+            <About data={landingPageData.About} />
+            {/* 팀 소개 */}
+            <Info data={landingPageData.Info} />
+            {/* 서비스 소개 */}
+            <Services data={landingPageData.Services} />
+            {/* 신청 */}
+            <Request data={landingPageData.Request} />
+            {/* 문의 */}
+            <Contact data={landingPageData.Contact} />
+          </>
+        );
+      case "admin":
+        return (
+          <>
+            <Admin data={landingPageData.Admin} />
+          </>
+        );
+    }
+  };
+
   return (
     <div>
       <Navigation />
-      {/* 메인 화면 */}
-      <Header data={landingPageData.Header} />
-      {/* 회사 소개 */}
-      <About data={landingPageData.About} />
-      {/* 팀 소개 */}
-      <Info data={landingPageData.Info} />
-      {/* 서비스 소개 */}
-      <Services data={landingPageData.Services} />
-      {/* 신청 */}
-      <Request data={landingPageData.Request} />
-      {/* 문의 */}
-      <Contact data={landingPageData.Contact} />
+      {renderContent()}
     </div>
   );
 };
